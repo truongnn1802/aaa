794729605e63bd4a5529731eea5c5c768715a3c4
truongnn1802 <truongnn2081@gmail.com>
2023-02-16 14:10:07 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+  },[])
   return (
     <div className="App">
       <header className="App-header">
